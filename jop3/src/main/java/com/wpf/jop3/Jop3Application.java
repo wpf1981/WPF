536d6ef746de09d61e84be6da15995f967441541
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2018-12-23 13:30:38 +0800

    初始化，链接数据库

---
+++ jop3/src/main/java/com/wpf/jop3/Jop3Application.java
@@ -0,0 +1,14 @@
+package com.wpf.jop3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jop3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jop3Application.class, args);
+    }
+
+}
+
